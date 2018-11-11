30bc1a00a76d766743ab41df7b057394cd01d357
afunderburg <afunderburg@connectwise.com>
2018-11-10 21:00:20 -0500

    Loading with configuration screen. Now its time to start jackin it up

--- dashboard-item-tutorial/src/main/resources/js/dashboard-item-tutorial.js
+++ dashboard-item-tutorial/src/main/resources/js/dashboard-item-tutorial.js
@@ -4,16 +4,16 @@ define('dashboard-items/tutorial', ['underscore', 'jquery', 'wrm/context-path'],
         this.issues = [];
     };
     /**
-    * Called to render the view for a fully configured dashboard item.
-    *
-    * @param context The surrounding <div/> context that this items should render into.
-    * @param preferences The user preferences saved for this dashboard item (e.g. filter id, number of results...)
-    */
+     * Called to render the view for a fully configured dashboard item.
+     *
+     * @param context The surrounding <div/> context that this items should render into.
+     * @param preferences The user preferences saved for this dashboard item (e.g. filter id, number of results...)
+     */
     DashboardItem.prototype.render = function (context, preferences) {
         this.API.showLoadingBar();
         var $element = this.$element = $(context).find("#dynamic-content");
         var self = this;
-        this.requestData().done(function (data) {
+        this.requestData(preferences).done(function (data) {
             self.API.hideLoadingBar();
             self.issues = data.issues;
             if (self.issues === undefined || self.issues.length  === 0) {
@@ -25,19 +25,53 @@ define('dashboard-items/tutorial', ['underscore', 'jquery', 'wrm/context-path'],
             self.API.resize();
             $element.find(".submit").click(function (event) {
                 event.preventDefault();
-                self.render(element, preferences);
+                self.render(context, preferences);
             });
         });
 
         this.API.once("afterRender", this.API.resize);
     };
 
-    DashboardItem.prototype.requestData = function () {
+    DashboardItem.prototype.requestData = function (preferences) {
         return $.ajax({
             method: "GET",
-            url: contextPath() + "/rest/api/2/search?jql=due<=3d"
+            url: contextPath() + "/rest/api/2/search?maxResults=10&jql=due<=" + preferences['due-date-input']
         });
     };
+
+    DashboardItem.prototype.renderEdit = function (context, preferences) {
+        var $element = this.$element = $(context).find("#dynamic-content");
+        $element.empty().html(Dashboard.Item.Tutorial.Templates.Configuration());
+        this.API.once("afterRender", this.API.resize);
+        var $form = $("form", $element);
+        $(".cancel", $form).click(_.bind(function() {
+            if(preferences['due-date-input'])
+                this.API.closeEdit();
+        }, this));
+
+        $form.submit(_.bind(function(event) {
+            event.preventDefault();
+
+            var preferences = getPreferencesFromForm($form);
+            var regexp = /^\d+([dwm])$/;
+            if(regexp.test(preferences['due-date-input'])) {
+                this.API.savePreferences(preferences);
+                this.API.showLoadingBar();
+            }
+        }, this));
+    };
+
+    function getPreferencesFromForm($form) {
+        var preferencesArray = $form.serializeArray();
+        var preferencesObject = {};
+
+        preferencesArray.forEach(function(element) {
+            preferencesObject[element.name] = element.value;
+        });
+
+        return preferencesObject;
+    }
+
 
     return DashboardItem;
 });
